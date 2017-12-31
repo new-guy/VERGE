eaaeadf69f798d756c60ab5f76f468092ec8f0fe
Robert McDonald <BobbyMcWho@users.noreply.github.com>
2017-12-31 00:09:02 -0500

    Fix mac build failure in CI
    Specify db version
    
    brew install berkeley-db4@4.8 will install a newer version than what is required.
    
    don't force the bdb link to reset

--- building/mac/requirements.sh
+++ building/mac/requirements.sh
@@ -1,8 +1,9 @@
 #brew install boost pkg-config 
 brew uninstall qt5
-brew install protobuf miniupnpc openssl qrencode berkeley-db4 
+brew install protobuf miniupnpc openssl qrencode berkeley-db4@4.8
+brew link berkeley-db4 --force
 # Might need these later: libevent librsvg
 curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/fdfc724dd532345f5c6cdf47dc43e99654e6a5fd/Formula/qt5.rb
-brew install ./qt5.rb
-brew link --force qt5
+HOMEBREW_NO_AUTO_UPDATE=1 brew install ./qt5.rb
+brew link qt5 --force
 
